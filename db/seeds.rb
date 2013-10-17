f87e90d55f4e16633b86e3ed303f48c064b324e1
Henry Nichols <henry.patch@gmail.com>
2013-10-17 13:58:43 -0500

    Fixed order of operations
    Some of the operations were out of order.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
 require 'faker'
 
-# create a few users
-User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :password => 'password'
-5.times do
-  User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password'
-end
-new_user.skills << Skill.where(id: rand(1..(computer_skills.length + design_skills.length)))
-
 # create a few technical skills
 computer_skills = %w(Ruby Sinatra Rails JavaScript jQuery HTML CSS)
 computer_skills.each do |skill|
@@ -17,6 +10,12 @@ end
 design_skills = %w(Photoshop Illustrator Responsive-Design)
 design_skills.each do |skill|
   Skill.create :name => skill, :context => 'creative'
+end
+# create a few users
+User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :password => 'password'
+5.times do
+ new_user =  User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password'
+ new_user.skills << Skill.where(id: rand(1..(computer_skills.length + design_skills.length)))
 end
 
 # TODO: create associations between users and skills
